15808da43c4618ea2947a76b3a8cd4feaf56b246
Aaryan Swarup <aaryan-swarup@hotmail.co.uk>
2025-12-03 16:25:34 +0000

    Fix TypeScript ease tuple type for Framer Motion

--- app/components/SocialProof.tsx
+++ app/components/SocialProof.tsx
@@ -46,7 +46,7 @@ const itemVariants = {
     y: 0,
     transition: {
       duration: 0.4,
-      ease: [0.25, 0.1, 0.25, 1],
+      ease: [0.25, 0.1, 0.25, 1] as [number, number, number, number],
     },
   },
 };
